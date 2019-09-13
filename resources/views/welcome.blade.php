cd0b3b3bea68aff712a5d6af51ff125e9f25af05
Your Name <natynaveros@gmail.com>
2019-09-13 15:00:26 -0500

    cambio en el titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Proyecto FTT
                 </div>
 
                 <div class="links">
