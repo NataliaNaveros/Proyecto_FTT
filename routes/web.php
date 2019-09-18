87bedbd42b6a004ec4310c4b9d72dcbcf943f051
Your Name <natynaveros@gmail.com>
2019-09-18 16:37:47 -0500

    Cambios Rutas

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Routw::get('auth/login','Auth\LoginController@getLogin');
+
 Route::resource('admin', 'AdminController');
+
